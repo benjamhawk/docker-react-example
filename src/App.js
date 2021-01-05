b056b48d2537f4bb270cdc10593c227474388c86
Benjamin Hawkins <benjamhawk@gmail.com>
2021-01-04 20:48:11 -0600

    Fix test again

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        ></a>
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
